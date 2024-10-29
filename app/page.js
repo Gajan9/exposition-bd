aa61ce69da2926f9bd0d6e6fa0f789bba72cf22f
GajanBaskaran <gajanbaskaran@gmail.com>
2024-10-29 21:33:00 +0100

    update

--- app/page.js
+++ app/page.js
@@ -3,16 +3,27 @@ import Link from "next/link";
 
 export default function HomePage() {
   return (
-    <div className="bg-[url('/img/bg-accueil.png')] bg-cover bg-center h-screen w-screen flex flex-col items-center justify-center">
-      <h1 className="text-black text-4xl flex uppercase">
-        <span className="border-2 border-black px-4 py-2 mx-1">La</span>
-        <span className="border-2 border-black px-4 py-2 mx-1">bande</span>
-        <span className="border-2 border-black px-4 py-2 mx-1">dessinée</span>
+    <div className="bg-[url('/img/bg-accueil.png')] bg-cover bg-center h-screen w-screen flex flex-col items-center">
+      <h1 className="text-black text-4xl flex flex-col uppercase items-center">
+        <div className="flex font-bangers">
+          <span className="border-2 border-black px-4 py-2 mx-1 font-bangers">
+            La
+          </span>
+          <span className="border-2 border-black px-4 py-2 mx-1 font-bangers">
+            bande
+          </span>
+        </div>
+        <div className="flex">
+          <span className="border-2 border-black px-4 py-2 mx-1 font-bangers">
+            dessinée
+          </span>
+        </div>
       </h1>
 
-      <div className="">
+      <div className="mx-4 max-w-screen-xl">
         <video controls src="/video/videoBD.mp4"></video>
       </div>
+
       <Link href="/asiatique" className="text-white">
         asiatique
       </Link>
--- tailwind.config.js
+++ tailwind.config.js
@@ -11,6 +11,9 @@ module.exports = {
         background: "var(--background)",
         foreground: "var(--foreground)",
       },
+      fontFamily: {
+        bangers: ["Bangers", "sans-serif"],
+      },
     },
   },
   plugins: [],
